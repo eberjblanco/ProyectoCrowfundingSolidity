a542cde2f5a63cf592b950c15e50a3ab70897b32
eberjblanco <eberj.blanco@gmail.com>
2022-04-20 12:32:22 -0400

    ult

---
+++ .workspaces/default_workspace/contracts/5_Crowdfunding.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Crowdfunding {
+
+    address private owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier isOwner(){
+        require(owner == msg.sender);
+        _;
+    }
+
+    string public nombre;
+    string public apellido;
+
+    uint public objetivo = 10 ether;
+    uint public balance;
+    uint public totalRecaudado;
+
+    address payable public artista = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+
+    function setMecenas(
+        string memory nombre_mecenas,
+        string memory apellido_mecenas
+    ) public isOwner payable {
+        nombre = nombre_mecenas;
+        apellido = apellido_mecenas;
+
+        require(msg.value > 1 ether);
+
+        balance = balance + msg.value;
+
+        if(balance >= objetivo){
+            payOut();
+        }
+    }
+
+    function getMecenas() public view returns(string memory,string memory){
+        return (nombre,apellido);
+    }
+
+    function payOut() private {
+        totalRecaudado = balance;
+        balance = 0;
+        artista.transfer(totalRecaudado);
+    }
+}
